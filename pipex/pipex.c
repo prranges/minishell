05b65495765fedd7debd4d0220fd21f001a93470
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2021-12-14 21:44:29 +0300

    pipex now supports one or more commands

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -16,10 +16,8 @@ char	**find_path(t_env *env)
 {
 	t_env	*p;
 	char 	**path;
-	int		i;
 
 	p = env;
-	i = 0;
 	while (p != NULL)
 	{
 		if (ft_strncmp(p->key, "PATH", 4) == 0)
@@ -30,177 +28,180 @@ char	**find_path(t_env *env)
 	return (path);
 }
 
-char *create_path(t_arg *data, char **all_paths)
+char *create_cmd_path(t_arg *data, char **all_paths, char *cmd)
 {
-	int i;
 	char *temp;
 	char *path;
 	(void)data;
+	char **str;
 
-	i = 1;
-
-	//printf("111\n");
-	while (*all_paths)
+	str = all_paths;
+	while (*str)
 	{
-		//printf("111\n");
-		temp = ft_strjoin(*all_paths, "/");
-		//printf("222\n");
-		path = ft_strjoin(temp, data->tokens->cmd[0]);
-		//printf("%s\n", path);
+		temp = ft_strjoin(*str, "/");
+		path = ft_strjoin(temp, cmd);
 		if (access(path, X_OK) != -1)
 			return (path);
-		//free(temp);
-		//free(path);
-		all_paths++;
+		free(temp);
+		free(path);
+		str++;
 	}
 	return (NULL);
 }
 
-//void close_fd(int fd)
+void	close_fds(t_arg *data, int **fd)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->num)
+	{
+		close(fd[i][0]);
+		close(fd[i][1]);
+		i++;
+	}
+}
+
+char *get_cmd_arg(int i, t_arg *data, char **env, char **cmd)
+{
+	(void)data;
+	(void)i;
+	(void)env;
+	//int str_num;
+	char **all_paths;
+	char *path_executive;
+
+	all_paths = find_path(data->env);//все пути
+//	for (int j = 0; j != 10; j++)
+//		printf("%s\n", all_paths[j]);
+	path_executive = create_cmd_path(data, all_paths, cmd[0]);
+	if (!path_executive)
+	{
+		perror("Error");
+		exit(EXIT_FAILURE);
+	}
+	printf("cmd - %s\n", path_executive);
+//	printf("PATH -- %s\n", path_executive);
+//		execve(path_executive, &cmd[i], env);
+	return (path_executive);
+}
+
+void child_process(int i, t_arg *data, int **fd, char **env, t_token *token)
+{
+	char *cmd_ex;
+//	(void)env;
+
+		if (i == 0)
+		{
+			cmd_ex = get_cmd_arg(i, data, env, token->cmd);
+//			if (!file){}
+//			printf("fd before - %d\n", file);
+//			printf("fd out - %d\n", fd_out);
+//			fd2 = open(cmd_ex, O_WRONLY);
+		//	fd[i][0] =  open(cmd_ex, O_WRONLY);
+//			dup2(fd2, 0);
+			dup2(fd[i][0], STDIN_FILENO);
+			//dup2(fd[i][1], STDOUT_FILENO);
+		//	close_fds(data, fd);
+			printf("cmd_ex - %s\n", cmd_ex);
+			printf("&cmd - %s\n", token->cmd[0]);
+			execve(cmd_ex, token->cmd, env);
+		}
+		else if (i == data->num - 1)
+		{
+			cmd_ex = get_cmd_arg(i, data, env, token->cmd);
+//			dup2(fd[i - 1][0], STDOUT_FILENO);//input
+//			dup2(fd[i][1], STDOUT_FILENO);//output
+//			printf("222\n");
+//			close_fds(data, fd);
+			execve(cmd_ex, token->cmd, env);
+		}
+		else
+		{
+			cmd_ex = get_cmd_arg(i, data, env, token->cmd);
+//			cmd_ex = create_cmd_path(data, path);
+//			fd[i][0] = open(cmd_ex, O_WRONLY);
+//			dup2(fd[i - 1][0], STDIN_FILENO);//input
+//			dup2(fd[i][1], STDOUT_FILENO);//output
+			//close_fds(data, fd);
+			execve(cmd_ex, token->cmd, env);
+		}
+}
+
+//char *make_cmd() {
+//	path = find_path(data->env);
+//	path_executive = create_cmd_path(data, path);
+//}
 
 int pipex(int argc, char **argv, char **env, t_arg *data)
 {
 	(void)argv;
+	(void)argc;
 
-	int **fd;
-	int fd2;
-	char **path;
-	char *path_exec;
-	pid_t *pid;
-	int i;
-	//int j = 0;
-	//int file;
-	//int fd_out;
-	char *flag;
+	int		**fd;
+	//char	*cmd_ex;
+	t_token	*node;
+	char	**cmd;
+//	int fd2 = 0;
+//	char	**path;
+//	char	*path_executive;
+	pid_t	*pid;
+	int		i;
 
 	i = 0;
-    errno = 0;
-	flag = "-l";
-	fd = malloc(sizeof(int *) * (argc - 4));
-	while(i < argc - 4)
+	fd = malloc(sizeof(int *) * data->num);
+	while(i < data->num)
 	{
 		fd[i] = malloc(sizeof(int) * 2);
+		if (!(fd[i]))
+		{
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
 		i++;
 	}
 	i = 0;
-	while (i < argc - 4)
+	cmd = data->tokens->cmd;
+	node = data->tokens;
+	while (i < data->num)
 	{
 		pipe(fd[i]);
-		if (pipe(fd[i]) == -1)
-			exit(1);
-		//printf("fd_1 - %d , %d\n", fd[i][0], fd[i][1]);
+		if (pipe(fd[i]) == -1) {
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
 		i++;
 	}
-	pid = malloc(sizeof(pid_t *));
+	pid = malloc(sizeof(pid_t *) * data->num);
+	//path = find_path(data->env);
+	//path_executive = create_cmd_path(data, path);
 	i = 0;
-	path = find_path(data->env);
-//	while (*path)
-//	{
-//		printf("%s\n", *path);
-//		path++;
-//	}
-	path_exec = create_path(data, path);
-	//printf("%s\n", path_exec);
 	while(i < data->num)
 	{
 		pid[i] = fork();
 		if (pid[i] < 0)
 		{
-			perror("minishell");
+			perror("Error");
 			exit(1);
 		}
-		else if (pid[i] == 0)
+		if (pid[i] == 0) //child process
 		{
-			//printf("111\n");
-			if (i == 0)
-			{
-//				char **arr;
-//				arr = malloc(sizeof(char *) * 2);
-//				arr[0] = malloc(sizeof(char) * 8);
-//				arr[1] = NULL;
-//				arr[0] = "/bin/pwd";
-//				file = open("/Users/caniseed/Desktop/minishell/minishell/minishell/pipex/input.txt", O_WRONLY);
-//				if (!file){}
-				//printf("fd before - %d\n", file);
-//				fd_out = open("/Users/caniseed/Desktop/minishell/minishell/minishell/pipex/output.txt", O_WRONLY | O_CREAT | O_TRUNC, \
-//			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-				//printf("fd out - %d\n", fd_out);
-				fd2 = open(path_exec, O_WRONLY);
-				dup2(fd2, 1);
-				//printf("fd after - %d\n", file);
-				//dup2(fd_out, 1);
-			//	printf("111\n");
-//				close(fd2);
-				execve(path_exec, data->tokens->cmd, env);
-			}
-			else
-			{
-				dup2(fd[i][0], 0);//input
-				dup2(fd[i][1], 1);//output
-				printf("222\n");
-//				close_fds(info, fd);
-//				execve(cmd[0], cmd, env);
-				execve(path_exec, data->tokens->cmd, env);
-			}
+			//cmd_ex = get_cmd_arg(i, data, env, node->cmd);
+//			printf("%s\n", cmd_ex);
+			child_process(i, data, fd, env, node);
 		}
 		i++;
+		if (node->next)
+		{
+			node = node->next;
+		}
 	}
+	close_fds(data, fd);
 	i = 0;
-	while (i < data->num) {
+	while (i != data->num)
+	{
 		waitpid(pid[i], NULL, 0);
 		i++;
 	}
-	return (0);
-//	int fd[2];
-//
-//	if (pipe(fd) == -1)
-//		return (1);
-//
-//	int pid1 = fork();
-//	int pid2;
-//	if (pid1 > 0)
-//		pid2 = fork();
-//
-//	if (pid1 < 0)
-//		return (2);
-//	if (pid1 == 0) //child process 1 (ping)
-//	{
-//		dup2(fd[1], STDOUT_FILENO);
-//		close(fd[0]);
-//		close(fd[1]);
-//		execlp("ls", "ls", "-l", NULL);
-//	}
-//
-//	if (pid2 == 0) //child process 1 (ping)
-//	{
-//		dup2(fd[0], STDIN_FILENO);
-//		close(fd[0]);
-//		close(fd[1]);
-//		execlp("cat", "cat", "-e", NULL);
-//	}
-//	int i = 	while (i < q_of_pipes)
-//	{
-//		malloc fd;
-//		pipe(fd[i]);
-//		fork();
-//		i++;
-//	}0;
-//
-
-
-
-//	int pid2 = fork();
-//
-//	if (pid2 == 0)
-//	{
-//		dup2(fd[0], STDIN_FILENO);
-//		close(fd[0]);
-//		close(fd[1]);
-//		execlp("grep", "grep", "rtt", NULL);
-//	}
-
-
-//	waitpid(pid1, NULL, 0);
-//	waitpid(pid2, NULL, 0);
 	return (0);
 }
